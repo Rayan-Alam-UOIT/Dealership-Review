6e5e6deb723ce9252f6e3c87133ea8a2c9853b0d
Rayan Alam <rayan.alam@ontariotechu.net>
2025-06-17 16:47:35 -0400

    Update admin.py

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,27 @@
-# from django.contrib import admin
-# from .models import related models
+from django.contrib import admin
+from .models import CarMake, CarModel
 
+# CarModelInline class - allows editing CarModels directly from CarMake admin
+class CarModelInline(admin.StackedInline):
+    model = CarModel
+    extra = 1  # Number of empty forms to display
+    fields = ['name', 'model_type', 'year', 'engine', 'trim_level', 'base_price']
+    ordering = ['-year']
 
-# Register your models here.
+# CarModelAdmin class - customization for CarModel admin
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'car_make', 'model_type', 'year', 'base_price')
+    list_filter = ('car_make', 'model_type', 'year')
+    search_fields = ['name', 'car_make__name']
+    ordering = ['-year', 'car_make', 'name']
 
-# CarModelInline class
+# CarMakeAdmin class - includes CarModelInline for managing related models
+class CarMakeAdmin(admin.ModelAdmin):
+    inlines = [CarModelInline]
+    list_display = ('name', 'founded_year', 'headquarters')
+    search_fields = ['name', 'headquarters']
+    list_filter = ('founded_year',)
 
-# CarModelAdmin class
-
-# CarMakeAdmin class with CarModelInline
-
-# Register models here
+# Register models with their custom admin classes
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
